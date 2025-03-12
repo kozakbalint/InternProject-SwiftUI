bbfede792ae8474a48772caf8d4bb841da18caab
Bálint Kozák <balint.kozak@capgemini.com>
2025-03-12 11:27:38 +0100

    Initial Commit

---
+++ SwiftUIInternProject/SwiftUIInternProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInternProjectApp.swift
+//  SwiftUIInternProject
+//
+//  Created by Balint Kozak on 2025. 03. 12..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInternProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
